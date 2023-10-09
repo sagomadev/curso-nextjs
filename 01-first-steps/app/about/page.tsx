4c131a0b9e19a1a0427b2e4b26d50b44f9396888
Samuel Gómez <sagomadev@gmail.com>
2023-10-09 15:37:20 +0200

    Solucion del ejercicio propuesto

--- 01-first-steps/app/about/page.tsx
+++ 01-first-steps/app/about/page.tsx
@@ -1,12 +1,13 @@
 /*
   Con este ejemplo crear dos paginas mas
   en el proyecto.
+  ContactPage y PricingPage
  */
 export default function AboutPage() {
   return (
-    <div>
+    <>
       <h1>About</h1>
       <p>This is the about page</p>
-    </div>
+    </>
   );
 }
