14b6d15f4d8531f2087575337b7ff34b58e32450
Samuel Gómez <sagomadev@gmail.com>
2023-10-09 19:38:11 +0200

    Generacion de links dinamica en el servidor

--- 01-first-steps/components/navbar/Navbar.tsx
+++ 01-first-steps/components/navbar/Navbar.tsx
@@ -1,5 +1,11 @@
 import Link from 'next/link';
 
+const navItems = [
+  { path: '/about', label: 'About' },
+  { path: '/pricing', label: 'Pricing' },
+  { path: '/contact', label: 'Contact' },
+];
+
 export const Navbar = () => {
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
@@ -7,15 +13,11 @@ export const Navbar = () => {
         <span>Home</span>
       </Link>
       <div className='flex flex-row flex-1 justify-end'>
-        <Link className='mr-2' href='/about'>
-          About
-        </Link>
-        <Link className='mr-2' href='/pricing'>
-          Pricing
-        </Link>
-        <Link className='mr-2' href='/contact'>
-          Contact
-        </Link>
+        {navItems.map(({ path, label }) => (
+          <Link key={path} href={path}>
+            <span className='mr-2'>{label}</span>
+          </Link>
+        ))}
       </div>
     </nav>
   );
