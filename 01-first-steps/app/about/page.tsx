577c159aa777561970f0d7f8d818bcdf115ff853
Samuel Gómez <sagomadev@gmail.com>
2023-10-09 14:25:20 +0200

    Explicacion app routing

---
+++ 01-first-steps/app/about/page.tsx
@@ -0,0 +1,12 @@
+/*
+  Con este ejemplo crear dos paginas mas
+  en el proyecto.
+ */
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page</p>
+    </div>
+  );
+}
