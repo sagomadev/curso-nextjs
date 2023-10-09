5163de490779ae85ce6cb75ccc5d52f42bade8a2
Samuel Gómez <sagomadev@gmail.com>
2023-10-09 17:53:55 +0200

    navegacion con anclas no usar con Next

--- 01-first-steps/components/navbar/Navbar.tsx
+++ 01-first-steps/components/navbar/Navbar.tsx
@@ -2,6 +2,17 @@ export const Navbar = () => {
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
       <span>Home</span>
+      <div className='flex flex-row flex-1 justify-end'>
+        <a className='mr-2' href='/about'>
+          About
+        </a>
+        <a className='mr-2' href='/pricing'>
+          Pricing
+        </a>
+        <a className='mr-2' href='/contact'>
+          Contact
+        </a>
+      </div>
     </nav>
   );
 };
